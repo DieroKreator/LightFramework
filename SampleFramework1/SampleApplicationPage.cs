bcb506a4f3af6e30dd8dcfcba029cc1e89370653
DieroKreator <qtechwired@gmail.com>
2024-04-01 11:02:49 -0300

    Created SampleApplicationPage file

---
+++ SampleFramework1/SampleApplicationPage.cs
@@ -0,0 +1,33 @@
+using OpenQA.Selenium;
+
+namespace SampleFramework1
+{
+    internal class SampleApplicationPage
+    {
+        public IWebDriver Driver { get; set; }
+
+        public SampleApplicationPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public bool IsVisible { 
+            get 
+            {
+                return Driver.Title.Contains("Sample Application Lifecycle - Sprint 1 - Ultimate QA");
+
+            } 
+            internal set {} 
+        }
+
+        internal void GoTo()
+        {
+            Driver.Navigate().GoToUrl("https://www.ultimateqa.com/sample-application-lifecycle-sprint-1/");
+        }
+
+        internal UltimateQAHomePage FillOutFormAndSubmit(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
