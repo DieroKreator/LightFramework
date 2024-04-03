f1e427ace71e2c9eae71c2d6aa247250f5f5a4f3
DieroKreator <qtechwired@gmail.com>
2024-04-03 12:40:03 -0300

    Added Explicit wait for the UltimateQAHomePage

--- SampleFramework1/UltimateQAHomePage.cs
+++ SampleFramework1/UltimateQAHomePage.cs
@@ -1,4 +1,6 @@
+using System;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
 namespace SampleFramework1
 {
@@ -12,7 +14,22 @@ namespace SampleFramework1
         }
 
         public bool IsVisible => FreeDiscoveryButton.Displayed;
-
+        // public bool IsVisible{
+        //     get 
+        //     {
+        //         try
+        //         {
+        //             WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+        //             IWebElement FreeDiscoveryButton = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("//a[@href='/selenium-java']")));
+        //             return FreeDiscoveryButton.Displayed;
+        //         }
+        //         catch (System.Exception)
+        //         {
+                    
+        //             throw;
+        //         }
+        //     }
+        // }
         public IWebElement FreeDiscoveryButton => Driver.FindElement(By.LinkText("SCHEDULE A FREE DISCOVERY SESSION"));
     }
 }
