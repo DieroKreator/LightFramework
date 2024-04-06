46792cba91a7f228c218805acb1ff1680fa9ea0a
DieroKreator <qtechwired@gmail.com>
2024-04-06 11:00:53 -0300

    Created a 2nd test for Sprint #2

--- SampleFramework1/SampleApplicationOneTests.cs
+++ SampleFramework1/SampleApplicationOneTests.cs
@@ -23,6 +23,20 @@ public class SampleApplicationOneTests
         Driver.Quit();
     }
 
+    [TestMethod]
+    public void PretendTestNumber2()
+    {
+        Driver = GetChromeDriver();
+        var sampleApplicationPage = new SampleApplicationPage(Driver);
+        sampleApplicationPage.GoTo();
+        Assert.IsTrue(sampleApplicationPage.IsVisible, "Same application was not visible.");
+
+        var ultimateQAHomePage = sampleApplicationPage.FillOutFormAndSubmit("Alex");
+        Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
+        Driver.Close();
+        Driver.Quit();
+    }
+
     private IWebDriver GetChromeDriver()
     {
         //var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
