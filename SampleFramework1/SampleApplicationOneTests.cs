bb7b1a774a241777edb9f7389c9c241a273d911c
DieroKreator <qtechwired@gmail.com>
2024-10-16 19:15:47 -0300

    Refactoring out assertions

--- SampleFramework1/SampleApplicationOneTests.cs
+++ SampleFramework1/SampleApplicationOneTests.cs
@@ -8,30 +8,39 @@ namespace SampleFramework1;
 public class SampleApplicationOneTests
 {
     private IWebDriver? Driver { get; set; }
+    internal SampleApplicationPage SampleAppPage { get; private set; }
     internal TestUser? TheTestUser { get; private set; }
 
     [TestMethod]
     [Description("Validate that user is able to fill out the form successfully using valid data.")]
     public void Test1()
     {
-        var sampleApplicationPage = new SampleApplicationPage(Driver);
-        sampleApplicationPage.GoTo();
-        var ultimateQAHomePage = sampleApplicationPage.FillOutFormAndSubmit(TheTestUser);
-        Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible.");
+        TheTestUser.GenderType = Gender.Female;
+        SampleAppPage.GoTo();
+        var ultimateQAHomePage = SampleAppPage.FillOutFormAndSubmit(TheTestUser);
+        AssertPageVisible(ultimateQAHomePage);
     }
 
     [TestMethod]
     [Description("Fake 2nd test.")]
     public void PretendTestNumber2()
     {
-        var sampleApplicationPage = new SampleApplicationPage(Driver);
-        sampleApplicationPage.GoTo();
-        var ultimateQAHomePage = sampleApplicationPage.FillOutFormAndSubmit(TheTestUser);
-        Assert.IsFalse(!ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
+        SampleAppPage.GoTo();
+        var ultimateQAHomePage = SampleAppPage.FillOutFormAndSubmit(TheTestUser);
+        AssertPageVisibleVariation2(ultimateQAHomePage);
     }
 
     // Sprint 3 - Sample Application Lifecycle
     // More data added
+    [TestMethod]
+    [Description("Validate that when selecting the Other gender type, the form is submitted successfully.")]
+    public void Test3()
+    {
+        TheTestUser.GenderType = Gender.Other;
+        SampleAppPage.GoTo();
+        var ultimateQAHomePage = SampleAppPage.FillOutFormAndSubmit(TheTestUser);
+        AssertPageVisibleVariation2(ultimateQAHomePage);
+    }
 
     [TestCleanup]
     public void CleanUpAfterEveryTestMethod()
@@ -44,6 +53,7 @@ public class SampleApplicationOneTests
     public void SetupForEverySingleTestMethod()
     {
         Driver = GetChromeDriver();
+        SampleAppPage = new SampleApplicationPage(Driver);
         TheTestUser = new TestUser();
         TheTestUser.FirstName = "Alex";
         TheTestUser.LastName = "Bullah";
@@ -54,6 +64,16 @@ public class SampleApplicationOneTests
     {
         //var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         return new ChromeDriver();
+    }
+
+    private static void AssertPageVisible(UltimateQAHomePage ultimateQAHomePage)
+    {
+        Assert.IsTrue(ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible.");
+    }
+
+    private static void AssertPageVisibleVariation2(UltimateQAHomePage ultimateQAHomePage)
+    {
+        Assert.IsFalse(!ultimateQAHomePage.IsVisible, "UltimateQA home page was not visible");
     }
 
 }
